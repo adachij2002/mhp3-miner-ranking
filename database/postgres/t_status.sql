26f494324dc5fb571a1231bae354a02f90fb1a68
adachij2002 <adachij2002@yahoo.co.jp>
2014-02-02 17:16:25 +0900

    Don't use sequence in database

--- database/postgres/t_status.sql
+++ database/postgres/t_status.sql
@@ -1,14 +1,10 @@
 DROP TABLE IF EXISTS t_status;
-DROP SEQUENCE IF EXISTS t_status_id_seq;
-
-CREATE SEQUENCE t_status_id_seq INCREMENT 1 START 1;
 
 CREATE TABLE t_status ( 
-	id bigint DEFAULT NEXTVAL('t_status_id_seq'::TEXT) NOT NULL,
 	checked boolean,
 	user_id text,
 	amulet_id text,
-	CONSTRAINT pk_t_status PRIMARY KEY (id),
+	CONSTRAINT pk_t_status PRIMARY KEY (user_id, amulet_id),
 	CONSTRAINT fk_t_status_m_amulet FOREIGN KEY (amulet_id) REFERENCES m_amulet (id),
 	CONSTRAINT fk_t_status_t_user FOREIGN KEY (user_id) REFERENCES t_user (id)
 );
